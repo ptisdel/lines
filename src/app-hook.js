b43ad679ae1b909d3b50b2309cfcd1b41d129467
Patrick Tisdel <patricktisdel@outlook.com>
2021-01-29 22:12:36 -0500

    (feat): add autosave countdown to avoid DDOSing google

--- src/app-hook.js
+++ src/app-hook.js
@@ -1,12 +1,13 @@
 import _ from 'lodash-es';
 import { useEffect, useState } from 'react';
 import { v4 } from 'uuid';
-import { matchesFilterTag } from './helpers';
+import { matchesFilterTag, useInterval } from './helpers';
 import * as constants from './constants';
 
 // used to identify what changes to storage originate here
 const instanceId = v4();
 
+// initial data
 const defaultNotes = {
   0: {
     tags: [],
@@ -19,29 +20,37 @@ const defaultNotes = {
     lines: [],
   },
 };
-const defaultNotesData = JSON.stringify(defaultNotes);
+const defaultData = JSON.stringify({
+  lastUpdaterId: instanceId,
+  notes: defaultNotes,
+});
 
 const saveData = notes => {
-  const notesData = JSON.stringify(notes);
+  const data = JSON.stringify({
+    notes,
+    lastUpdaterId: instanceId,
+  });
   if (constants.CHROME_EXTENSION_MODE) {
     // eslint-disable-next-line no-undef
-    chrome.storage.sync.set({ instanceId, notesData }, () => {});
+    chrome.storage.sync.set({ data }, () => {});
+    console.log('saving');
     return;
   }
   
-  window.localStorage.setItem('notesData', notesData);
+  window.localStorage.setItem('data', data);
 }
 
 const loadData = callback => {
   if (constants.CHROME_EXTENSION_MODE) {
     // eslint-disable-next-line no-undef
-    chrome.storage.sync.get({'notesData': defaultNotesData}, result => {
-      callback(result.notesData);
+    chrome.storage.sync.get({ data: defaultData }, result => {
+      const parsedData = JSON.parse(result.data);
+      callback(parsedData.notes);
     });
     return;
   }
 
-  callback(window.localStorage.getItem('notesData'));
+  callback(window.localStorage.getItem('data'));
 }
 
 export const useApp = () => {
@@ -53,27 +62,25 @@ export const useApp = () => {
   const [filterTag, setFilterTag] = useState(null);
 
   // load data
-  
+
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    const callback = notesData => {
-      const notesDataParsed = JSON.parse(notesData);
-      const noteIndices = _.keys(notesDataParsed);
-      const firstNoteIndex = _.head(noteIndices);
-      setCurrentNoteIndex(firstNoteIndex);
-      setNotes(notesDataParsed);
+    const callback = notes => {
+      setNotes(notes);
       setIsLoading(false);
     }
 
+    // load initialize 
     loadData(callback);
 
     if (!constants.CHROME_EXTENSION_MODE) return;
 
     // if there are changes, and those changes include notesData, load notesData
     const listenForChanges = changes => {
-      const saverInstanceId = changes?.instanceId;
-      const newNotesData = changes?.notesData?.newValue;
-      if (saverInstanceId === instanceId && newNotesData) callback(newNotesData)
+      const parsedData = JSON.parse(changes?.data.newValue);
+      const latestUpdaterId = parsedData.lastUpdaterId;
+      const newNotes = parsedData.notes;
+      if (latestUpdaterId !== instanceId && newNotes) callback(newNotes)
     };
     
     /* eslint-disable no-undef */
@@ -82,12 +89,14 @@ export const useApp = () => {
     /* eslint-enable no-undef */
   }, []);
 
-  // save data
-
+  // select first available note
   useEffect(() => {
-    if (!isLoading) saveData(notes);
-  }, [isLoading, notes]);
-
+    if (notes === null) return;
+    const noteIndices = _.keys(notes);
+    const firstNoteIndex = _.head(noteIndices);
+    if (currentNoteIndex === null) setCurrentNoteIndex(firstNoteIndex);
+  }, [currentNoteIndex, notes]);
+  
   // get all lines
 
   const currentNote = _.get(notes, currentNoteIndex);
@@ -107,6 +116,20 @@ export const useApp = () => {
   // get visible text
 
   const visibleText = _.join(filterTag ? unfilteredLines : allLines, '\n');
+
+  // editing a note starts a countdown to autosave
+
+  const [saveCountdown, setSaveCountdown] = useState(null);
+  useInterval(() => {
+    if (_.isNull(saveCountdown)) return;
+
+    setSaveCountdown(saveCountdown - 1);
+
+    if (saveCountdown === 0) {
+      setSaveCountdown(null)
+      saveData(notes);
+    }
+  }, _.isNull(saveCountdown) ? null : 1000);
 
   const editNote = text => {
     const updatedLines = text?.split('\n');
@@ -135,14 +158,6 @@ export const useApp = () => {
     const dedupedTags = _.uniq(tags);
     const sortedTags = _.sortBy(dedupedTags);
 
-    // add new tags after remaining old tags, so the tags don't jump around in order
-    // const newTags = _.difference(dedupedTags, allTags);
-    // const remainingTags = _.difference(allTags, newTags);
-    // const updatedTags = [
-    //   ...remainingTags,
-    //   ...newTags,
-    // ];
-
     // if current filter no longer has matches, clear filter
     if (!_.includes(dedupedTags, filterTag)) setFilterTag(null);
 
@@ -157,7 +172,14 @@ export const useApp = () => {
       [currentNoteIndex]: updatedNote,
     };
     setNotes(newNotes);
+    setSaveCountdown(3);
   };
+
+  useEffect(() => {
+    return () => {
+      if (!isLoading) saveData(notes);
+    };
+  }, [isLoading, notes]);
 
   // filter notes by tags
 
@@ -221,6 +243,7 @@ export const useApp = () => {
         }
       };
       setNotes(newNotes);
+      saveData(newNotes);
       setShowCreateModal(false);
     }
   }
@@ -243,6 +266,7 @@ export const useApp = () => {
     const newNotes = _.omit(notes, [currentNoteIndex]);
     selectFirstNote(newNotes);
     setNotes(newNotes);
+    saveData(newNotes);
     setShowDeleteModal(false);
   }
 
--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,29 @@
 import _ from 'lodash-es';
+import { useEffect, useRef } from 'react';
 
 export const matchesFilterTag = (line, filterTag) => (
   _.includes(line.tags, filterTag)
   || line.tempTag === filterTag 
 );
+
+export const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  // eslint-disable-next-line
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
